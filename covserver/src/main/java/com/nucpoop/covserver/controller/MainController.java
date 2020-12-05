6ab884e63463f776a839c5b9779b7092547d2be9
nucpoop <kjm5546@gmail.com>
2020-12-06 06:47:55 +0900

    [add] api

--- covserver/src/main/java/com/nucpoop/covserver/controller/MainController.java
+++ covserver/src/main/java/com/nucpoop/covserver/controller/MainController.java
@@ -1,5 +1,23 @@
 package com.nucpoop.covserver.controller;
 
+import com.nucpoop.covserver.util.Utils;
+
+import java.io.IOException;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MainController {
+	
+	@RequestMapping(value ="/home")
+	public String home() {
+		return "index.html";
+	}
+	
+	@RequestMapping("/test")
+	public String test() throws IOException {
+		return Utils.getCovData().toString();
+	}
 
 }
