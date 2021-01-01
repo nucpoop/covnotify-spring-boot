1f1b059a5eb093ceafbb019f6e3381045a80f760
NucPoop <nucpoop@Jam-MacBookPro.local>
2021-01-01 21:10:32 +0900

    [fix] maincontroller

--- covserver/src/main/java/com/nucpoop/covserver/controller/MainController.java
+++ covserver/src/main/java/com/nucpoop/covserver/controller/MainController.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.nucpoop.covserver.util.Utils;
 
@@ -14,7 +13,7 @@ public class MainController {
 	
 	@RequestMapping("/")
 	public String hello() {
-		return "test";
+		return "user/test";
 	}
 	
 	@RequestMapping("/test")
