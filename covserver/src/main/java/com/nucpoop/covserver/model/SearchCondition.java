8c667f9ba4d777651514976681a60487f3f1d9b0
Nucpoop <nucpoop@Jam-Mac-mini.local>
2021-03-09 00:12:32 +0900

    [fix] lombok error

--- covserver/src/main/java/com/nucpoop/covserver/model/SearchCondition.java
+++ covserver/src/main/java/com/nucpoop/covserver/model/SearchCondition.java
@@ -1,5 +1,7 @@
 package com.nucpoop.covserver.model;
 
+import lombok.Builder;
+
 //import javax.persistence.Entity;
 
 import lombok.Getter;
@@ -11,9 +13,18 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Builder
 public class SearchCondition {
+	
 	int pageNo;
 	int numberOfRows;
 	String startCreateDt;
 	String endCreateDt;
+
+	public SearchCondition(int pageNo, int numberOfRows, String startCreateDt, String endCreateDt) {
+		this.pageNo = pageNo;
+		this.numberOfRows = numberOfRows;
+		this.startCreateDt = startCreateDt;
+		this.endCreateDt = endCreateDt;
+	}
 }
