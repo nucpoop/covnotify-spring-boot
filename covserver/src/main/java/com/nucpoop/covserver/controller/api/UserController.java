1c7a4ae364ccfdab0e7fbc9f6c208f9f695148d4
NucPoop <kjm5546@gmail.com>
2021-06-23 10:29:46 +0900

    test commit

--- covserver/src/main/java/com/nucpoop/covserver/controller/api/UserController.java
+++ covserver/src/main/java/com/nucpoop/covserver/controller/api/UserController.java
@@ -92,7 +92,7 @@ public class UserController {
 	@DeleteMapping("/withdrawal")
 	public ResponseEntity<?> withdrawalUser(@CurrentUser UserPrincipal currentUser,
 			@RequestBody PasswordRequest passwordRequest) {
-		//String password = passwordEncoder.encode(passwordRequest.getPassword());
+		// String password = passwordEncoder.encode(passwordRequest.getPassword());
 		User user = User.builder().email(currentUser.getUsername()).password(passwordRequest.getPassword()).build();
 
 		try {
